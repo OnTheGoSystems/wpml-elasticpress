595e3993926ed5f752030bb38da31975a786889b
Juan de Paco <juan.d@onthegosystems.com>
2023-04-25 10:46:37 +0200

    fix: ensure CLI command works
    Ref: wpmlbridge-268

--- src/Command.php
+++ src/Command.php
@@ -5,19 +5,30 @@ namespace WPML\ElasticPress;
 
 
 class Command extends \ElasticPress\Command {
+	// Extending the native class to add `--post-lang` argument
 
 	/**
-	 * I have to override it to add `--post-lang` argument
-	 *
 	 * Index all posts for a site or network wide
 	 *
 	 * @synopsis [--setup] [--network-wide] [--per-page] [--nobulk] [--show-errors] [--offset] [--indexables] [--show-bulk-errors] [--show-nobulk-errors] [--post-type] [--include] [--post-ids] [--ep-host] [--ep-prefix] [--post-lang]
-	 * @param  array  $args  Positional CLI args.
-	 * @param  array  $assoc_args  Associative CLI args.
+	 * @param array  $args  Positional CLI args.
+	 * @param array  $assoc_args  Associative CLI args.
 	 *
 	 * @since 0.1.2
+	 * @deprecated
 	 */
 	public function index( $args, $assoc_args ) {
-		return parent::index( $args, $assoc_args );
+		return parent::sync( $args, $assoc_args );
+	}
+
+	/**
+	 * Sync all posts for a site or network wide
+	 *
+	 * @synopsis [--setup] [--network-wide] [--per-page] [--nobulk] [--show-errors] [--offset] [--indexables] [--show-bulk-errors] [--show-nobulk-errors] [--post-type] [--include] [--post-ids] [--ep-host] [--ep-prefix] [--post-lang]
+	 * @param array  $args  Positional CLI args.
+	 * @param array  $assoc_args  Associative CLI args.
+	 */
+	public function sync( $args, $assoc_args ) {
+		return parent::sync( $args, $assoc_args );
 	}
 }
