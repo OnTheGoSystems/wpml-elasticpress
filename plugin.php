112db81e1b0f0d20966eb45f7f7cc6c9f0ba3f25
Juan de Paco <juan.d@onthegosystems.com>
2023-07-31 17:46:07 +0200

    fix: build package to deploy
    Ref: wpmlbridge-276

--- plugin.php
+++ plugin.php
@@ -5,17 +5,35 @@
  * Description: Add full WPML support for ElasticPress.
  * Author: OnTheGoSystems
  * Author URI: http://www.onthegosystems.com/
- * Version: 2.0.0
+ * Version: 2.0.1
  * Plugin Slug: wpmlelasticpress
  *
  * @package wpml/bridge/elasticpress
  */
 
-if ( defined( 'WPMLELASTICPRESS_VERSION' ) ) {
-    return;
+register_activation_hook( __FILE__, 'wpmlElasticPressDeleteVersionWithWrongSlug' );
+if ( ! function_exists( 'wpmlElasticPressDeleteVersionWithWrongSlug' ) ) {
+	function wpmlElasticPressDeleteVersionWithWrongSlug() {
+		// See https://onthegosystems.myjetbrains.com/youtrack/issue/wpmlbridge-276
+		$plugins    = get_plugins();
+		$pluginSlug = plugin_basename( __FILE__ );
+		foreach ( $plugins as $pluginId => $pluginData ) {
+			if (
+				'WPML ElasticPress' === $pluginData['Name']
+				&& $pluginId !== $pluginSlug
+			) {
+				deactivate_plugins( $pluginId );
+				delete_plugins( [ $pluginId ] );
+			}
+		}
+	}
 }
 
-define( 'WPMLELASTICPRESS_VERSION', '2.0.0' );
+if ( defined( 'WPMLELASTICPRESS_VERSION' ) ) {
+	return;
+}
+
+define( 'WPMLELASTICPRESS_VERSION', '2.0.1' );
 define( 'WPMLELASTICPRESS_PLUGIN_PATH', dirname( __FILE__ ) );
 
 require_once WPMLELASTICPRESS_PLUGIN_PATH . '/vendor/autoload.php';
